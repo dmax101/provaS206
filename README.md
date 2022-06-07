3962617a5a899a22e440330336a4cc6cba013df9
Danilo Ribeiro <danriba@gmail.com>
2022-06-06 21:04:23 -0300

    Prova

---
+++ README.md
@@ -0,0 +1,47 @@
+![Cypress.io](https://www.cypress.io/static/33498b5f95008093f5f94467c61d20ab/ac1e1/cypress-logo.webp)
+
+# Exercício Cypress - Teste de UI
+
+| Matricula | Nome           |
+| :-------: | -------------- |
+|   1411    | Danilo Ribeiro |
+
+Este projeto tem como objetivo testar a interface do usuário.
+
+## 🌐 Instalação
+
+Inicie clonando o projeto no seu computador.
+
+```bash
+git clone https://github.com/dmax101/provaS206
+```
+
+Logo após, execute o comando para instalar as dependências:
+
+```bash
+npm install
+```
+
+## 🏎️ Testes
+
+Logo após, execute o comando para iniciar o gerenciador de testes do cypress:
+
+```bash
+./node_modules/.bin/cypress open
+```
+
+Logo irá abrir uma janela onde será possível escolher o teste `prova.spec.js` que está na pasta `0-inatel`. Clique no arquivo e outra janela abrirá com o teste que executará automaticamente.
+
+🎌 Pronto! Agora você pode testar a interface do usuário.
+
+## 📒 Relatórios
+
+Para gerar um relatório e de quebra o vídeo do teste rodando no seu computador, execute o comando abaixo:
+
+```bash
+.\node_modules\.bin\cypress run --spec .\cypress\integration\0-inatel\prova.spec.js --browser chrome --reporter mochawesome
+```
+
+**_Vídeo_**: Assista o relatório [aqui](./cypress/videos/0-inatel/autoPractice.spec.js.mp4).
+
+**_Relatório_**: Acesse o relatório [aqui](./mochawesome-report/mochawesome.html).
---
+++ cypress/integration/0-inatel/prova.spec.js
@@ -0,0 +1,62 @@
+/// <reference types="Cypress" />
+
+describe("XYZ Bank suite test 01", () => {
+  it("deve ser possível realizar o login com o usuário Harry Potter", () => {
+    cy.visit(
+      "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login"
+    );
+    cy.get(".borderM > :nth-child(1) > .btn").click();
+    cy.get("#userSelect").select("Harry Potter");
+    cy.get("form.ng-valid > .btn").click();
+  });
+});
+describe("XYZ Bank suite test 02", () => {
+  beforeEach(() => {
+    cy.visit(
+      "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login"
+    );
+    cy.get(".borderM > :nth-child(1) > .btn").click();
+    cy.get("#userSelect").select("Harry Potter");
+    cy.get("form.ng-valid > .btn").click();
+  });
+
+  it("dado que o usuário Harry Potter esteja logado deve-se tentar retirar um valor maior do que tem na conta!", () => {
+    cy.get('[ng-class="btnClass3"]').click();
+    cy.get(".form-control").type(generateRandomInteger());
+    cy.get("form.ng-dirty > .btn").click();
+    cy.get(".error").should(
+      "contain",
+      "Transaction Failed. You can not withdraw amount more than the balance."
+    );
+  });
+
+  it("dado que o usuário Harry Potter esteja logado ele deve poder realizar um depósito em sua conta", () => {
+    cy.get('[ng-class="btnClass2"]').click();
+    let valor = generateRandomInteger();
+    cy.get(".form-control").type(valor);
+    cy.get("form.ng-dirty > .btn").click();
+    cy.get(".borderM > :nth-child(3) > :nth-child(2)").should("contain", valor);
+  });
+
+  it("dado que o usuário Harry Potter esteja logado e tenha depositado $500 ele poderá retirar todo o saldo.", () => {
+    cy.get('[ng-class="btnClass2"]').click();
+    cy.get(".form-control").type(500);
+    cy.get("form.ng-dirty > .btn").click();
+    cy.get(".borderM > :nth-child(3) > :nth-child(2)").should("contain", 500);
+
+    cy.get('[ng-class="btnClass3"]').click();
+
+    cy.wait(1000);
+    cy.get(".form-control").type("500");
+    cy.get("form.ng-dirty > .btn").click();
+    cy.get(".borderM > :nth-child(3) > :nth-child(2)").should("contain", 0);
+  });
+});
+
+function generateRandomInteger() {
+  const randomInteger =
+    Number.parseFloat(Math.random() * 10000).toFixed(0) + 100;
+  return randomInteger;
+}
+
+var array = [];
